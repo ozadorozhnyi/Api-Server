233e699a5ecef818c2477f689d2b45052ccfac99
Oleg Zadorozhnyi <ipfound@gmail.com>
2024-08-14 01:10:33 +0300

    routes for going next.

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('users', function () {
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'users list',
+    ]);
+});
+
+// implicit binding (eloquent model)
+Route::get('/users/{user}', function (\App\Models\User $user) {
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'get user info',
+    ]);
+});
+
+Route::post('users', function (\Illuminate\Http\Request $request) {
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'create a new user',
+    ]);
+});
+
+Route::patch('/users/{user}', function (\App\Models\User $user, \Illuminate\Http\Request $request) {
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'patch user'
+    ]);
+});
+
+Route::delete('/users/{user}', function (\App\Models\User $user) {
+    return new \Illuminate\Http\JsonResponse([
+        'data' => 'delete user'
+    ]);
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
